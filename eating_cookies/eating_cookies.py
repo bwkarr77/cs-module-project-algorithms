b483032d0e07c02bcb4e5222fbb718665f918fe8
Brett Karr <b.karr@polyclip.us>
2020-08-13 21:58:36 -0500

    got eating cookies optimized for the larger inputs

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -22,14 +22,21 @@ def eating_cookies(n):
         return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
 
 def fib_mem(n, cache = {}):
+    # print('recursion', n)
     if n == 1 or n == 0:
         return 1
     elif n < 0:
         return 0
+    # have we already done this recursion loop/calculation? IF:
+    # cache = {2: 2, 3: 4, 4: 7}
+    # if n = 4, then we return cache[4]=7
     elif n in cache.keys():
+        # print('cache: ', cache)
         return cache[n]
     else:
-        value = fib_mem(n-1) + fib_mem(n-2)
+        value = fib_mem(n-1) + fib_mem(n-2) + fib_mem(n-3)
+        # we create a dictionary of returned values (aka, recursion loop is complete). Looks like:
+        # {2: 2, 3: 4, 4: 7, 5: 13, 6: 24, 7: 44, ...}
         cache[n] = value
         return value
 
--- eating_cookies/test_eating_cookies.py
+++ eating_cookies/test_eating_cookies.py
@@ -4,16 +4,17 @@ from eating_cookies import eating_cookies, fib_mem
 class Test(unittest.TestCase):
 
   def test_eating_cookies_small_n(self):
-    self.assertEqual(eating_cookies(0), 1)
-    self.assertEqual(eating_cookies(1), 1)
-    self.assertEqual(eating_cookies(2), 2)
-    self.assertEqual(eating_cookies(5), 13)
-    self.assertEqual(eating_cookies(10), 274)
+    self.assertEqual(fib_mem(0), 1)
+    self.assertEqual(fib_mem(1), 1)
+    self.assertEqual(fib_mem(2), 2)
+    self.assertEqual(fib_mem(3), 4)
+    self.assertEqual(fib_mem(5), 13)
+    self.assertEqual(fib_mem(10), 274)
 
   def test_eating_cookies_large_n(self):
-    self.assertEqual(fib_mem(51), 10562230626642)
-    # self.assertEqual(eating_cookies(100, [0 for i in range(101)]), 180396380815100901214157639)
-    # self.assertEqual(eating_cookies(500, [0 for i in range(501)]), 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527)
+    self.assertEqual(fib_mem(50), 10562230626642)
+    self.assertEqual(fib_mem(100), 180396380815100901214157639)
+    self.assertEqual(fib_mem(500), 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527)
 
 
 if __name__ == '__main__':
